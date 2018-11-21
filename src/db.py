0509061567bdddde06cc52be93f69a24ee3e6b08
DerevenetsArtyom <artem@comvoy.be>
2018-11-21 22:10:50 +0200

    Add 'get_question' function to db.py

--- src/db.py
+++ src/db.py
@@ -1,10 +1,11 @@
 import aiopg.sa
+from .exeptions import RecordNotFound
 
 
 async def init_pg(app):
     """For making DB queries we need an engine instance"""
-    conf = app['config']['postgres']
-    engine = await aiopg.sa.create_engine(**conf)  # TODO: explicitly?
+    pg_config = app['config']['postgres']
+    engine = await aiopg.sa.create_engine(**pg_config)  # TODO: explicitly?
     app['db'] = engine
 
 
@@ -12,3 +13,21 @@ async def close_pg(app):
     """It is a good practice to close all resources on program exit"""
     app['db'].close()
     await app['db'].wait_closed()
+
+
+async def get_question(conn, question_id):
+    result = await conn.execute(
+        question.select().where(question.c.id == question_id)
+    )
+    question_record = await result.first()
+    if not question_record:
+        msg = f"Question with id: {question_id} does not exists"
+        raise RecordNotFound(msg)
+    result = await conn.execute(
+        choice.select()
+        .where(choice.c.id == question_id)
+        .order_by(choice.c.id)
+    )
+    choice_records = await result.fetchall()
+    return question_record, choice_records
+
