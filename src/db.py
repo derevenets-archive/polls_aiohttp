207009adcc69cefbc9f690affa2a2b8ac55053c4
DerevenetsArtyom <artem@comvoy.be>
2018-11-21 21:45:14 +0200

    Add startup and teardown db scripts

--- src/db.py
+++ src/db.py
@@ -1,3 +1,4 @@
+import aiopg.sa
 from sqlalchemy import (
     MetaData, Table, Column, ForeignKey,
     Integer, String, Date
@@ -19,10 +20,22 @@ choice = Table(
     Column('id', Integer, primary_key=True),
     Column('choice_text', String(200), nullable=False),
     Column('votes', Integer, server_default='0', nullable=False),
-
     Column(
         'question_id',
         Integer,
         ForeignKey('question.id', ondelete='CASCADE')
     )
 )
+
+
+async def init_pg(app):
+    """For making DB queries we need an engine instance"""
+    conf = app['config']['postgres']
+    engine = await aiopg.sa.create_engine(**conf)  # TODO: explicitly?
+    app['db'] = engine
+
+
+async def close_pg(app):
+    """It is a good practice to close all resources on program exit"""
+    app['db'].close()
+    await app['db'].wait_closed()
--- src/main.py
+++ src/main.py
@@ -2,9 +2,13 @@ from aiohttp import web
 
 from settings import config
 from routes import setup_routes
-
+from db import init_pg, close_pg
 
 app = web.Application()
+
+app.on_startup.append(init_pg)
+app.on_cleanup.append(close_pg)
+
 setup_routes(app)
 app['config'] = config
 web.run_app(app)
