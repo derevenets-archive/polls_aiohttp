c928c3deeb418be54a1b440c2b3df87c611e0a99
DerevenetsArtyom <artem@comvoy.be>
2018-11-22 20:14:57 +0200

    Create templates and handlers from 404 and 500 errors

---
+++ src/middlewares.py
@@ -0,0 +1,9 @@
+import aiohttp_jinja2
+
+
+async def handle_404(request):
+    return aiohttp_jinja2.render_template('404.html', request, {})
+
+
+async def handle_500(request):
+    return aiohttp_jinja2.render_template('500.html', request, {})
