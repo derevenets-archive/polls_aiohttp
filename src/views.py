93925fd31c9c682c19f34d232188907498d810db
DerevenetsArtyom <artem@comvoy.be>
2018-11-22 20:54:23 +0200

    Make 'index' view works with 'index.html' template

--- src/views.py
+++ src/views.py
@@ -1,16 +1,22 @@
 import aiohttp_jinja2
+import models
 from aiohttp import web
 from db import get_question
 from exceptions import RecordNotFound
 
 
+@aiohttp_jinja2.template('index.html')
 async def index(request):
-    return web.Response(text='Hello Aiohttp!')
+    async with request.app['db'].acquire() as conn:
+        cursor = await conn.execute(models.question.select())
+        records = await cursor.fetchall()
+        questions = [dict(q) for q in records]
+        return {'questions': questions}
 
 
 @aiohttp_jinja2.template('detail.html')
 async def poll(request):
-    async with request.app['db'].ascuire() as conn:
+    async with request.app['db'].acquire() as conn:
         question_id = request.match_info['question_id']
         try:
             question, choices = get_question(conn, question_id)
